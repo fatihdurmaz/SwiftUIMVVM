62be51f511e2fac46323a18493fd3a9689162402
Fatih Durmaz <71706970+fatihdurmaz@users.noreply.github.com>
2023-03-22 15:10:04 +0300

    Initial Commit

---
+++ AlamofireAPIServiceMVVM/AlamofireAPIServiceMVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlamofireAPIServiceMVVMApp.swift
+//  AlamofireAPIServiceMVVM
+//
+//  Created by Fatih Durmaz on 22.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct AlamofireAPIServiceMVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
